b6ace5734b82077e05cd297a00f3de5d9f5d5045
Katia Basmaci <katia.basmaci@gmail.com>
2020-04-27 18:08:59 +0000

    Add adopt app to settings

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's9!8p72z+q!*+tv1r_@u*dys@&g6@omx674^exoqtr&8-&6a82'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adopt',
 ]
 
 MIDDLEWARE = [
