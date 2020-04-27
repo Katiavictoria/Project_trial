d112e31bdd296a77e761caa4c747d26204744c1c
Katia Basmaci <katia.basmaci@gmail.com>
2020-04-27 19:56:54 +0000

    Add pet model

--- adopt/models.py
+++ adopt/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    CHOICES = (
+        ('red', 'Red'),
+        ('yellow', 'Yellow'),
+        ('black', 'Black'),
+    )
+
+    name = models.CharField('Name', max_length=50)
+
+    age = models.IntegerField('Age')
+
+    color = models.CharField('Color', max_length=50, choices=CHOICES)
