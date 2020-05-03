9325972df4d615d95afa22693559795c5e54a034
Katia Basmaci <katia.basmaci@gmail.com>
2020-05-03 02:18:44 +0000

    Add bootstrap.

--- adopt/views.py
+++ adopt/views.py
@@ -1,25 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Pet
 
+
 def index(request):
-    return HttpResponse('Hello Katia')
+    return HttpResponse('Hello, Katus!')
+
 
 def list_pets(request):
-    response= '<ul>'
-    
     pets = Pet.objects.all()
-
-    for pet in pets:
-    
-        response+= f'<li><a href="/adopt/{pet.id}">{pet.name}</a></li>'
-    response+= '</ul>'        
-
-    return HttpResponse(response)
+    return render(request, 'adopt/list.html', {'pets': pets})
 
 
 def get_pet(request, pet_id):
 
     pet = Pet.objects.get(id=pet_id)
 
-    return HttpResponse(f'Hello I am {pet.name} and I am Katias pet!') 
+    return HttpResponse(f'Hello, This is {pet.name}!') 
